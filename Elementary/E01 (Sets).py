a0c54b25054a0cb16ca4d0687856903149602e3d
Ankit Prasad <85994752+ankitprasad2005@users.noreply.github.com>
2023-10-12 08:48:36 +0530

    Update E01 (Sets).py

--- Elementary/E01 (Sets).py
+++ Elementary/E01 (Sets).py
@@ -11,7 +11,9 @@ st1.add(9)
 st1.remove(9)
 
 a = st1.intersection(st2)
+i = st1 | st2
 b = st1.union(st2)
+h = st1 & st2
 
 # bool
 c = st1.issubset(st3)
@@ -28,3 +30,16 @@ print (a,b,c,d,e,f, g)
 # to create copy of set (Shallow Copy)
 # st5 = st4[:] 
 st5 = st4.copy()
+
+st6 = {}
+print(type(st6))
+st7 = set()
+print(type(st7))
+
+#we can add multiple element using update
+st1.update([8,9])
+#discard won't show error if 8 does not exist
+sr1.discard(8)
+
+#concatenation
+print("_".join(st1)) 
